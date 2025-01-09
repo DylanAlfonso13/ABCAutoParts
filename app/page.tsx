d060ab74fa59081d6df5dfe46d0872a1e2131035
Dylan Alfonso <alfonso.d@wustl.edu>
2025-01-08 20:18:26 -0600

    Added test comment to test vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Nav from '../components/Nav';
 import Hero from '../components/index/Hero';
 import Cards from '../components/index/Cards';
 
+// Test comment
 export default function Home() {
   return (
     <div>
